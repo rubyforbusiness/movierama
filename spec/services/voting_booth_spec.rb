094c89c7dd0b0d6f6d298b8b39806d8184f8157a
Richard Brooke <ragnarcom@googlemail.com>
2021-02-16 12:09:10 +0000

    test incrementing user.hated_movies

--- spec/services/voting_booth_spec.rb
+++ spec/services/voting_booth_spec.rb
@@ -30,6 +30,64 @@ RSpec.describe VotingBooth do
         }.from(false).to(true)
       end
     end
+
+    context 'me hating a movie' do
+      subject do
+        described_class.new(user, movie).vote(:hate)
+      end
+
+      let!(:user) { create(:user) }
+      let!(:movie) { create(:movie, user: user) }
+
+      it 'increases the haters of a movie' do
+        expect {subject}.to change { movie.haters.count }.by 1
+      end
+
+      it 'makes me a hater of a movie' do
+        expect {subject}.to change {
+          movie.haters.include? user
+        }.from(false).to(true)
+      end
+
+      it 'increases the number of movies I hate' do
+        expect {subject}.to change { user.hated_movies.count }.by 1
+      end
+
+      it 'adds the movie to the list I hate' do
+        expect {subject}.to change {
+          user.hated_movies.include? movie
+        }.from(false).to(true)
+      end
+    end
+
+    context 'me hating a movie I used to like' do
+      subject do
+        described_class.new(user, movie).vote(:hate)
+      end
+
+      let!(:user) { create(:user) }
+      let!(:movie) { create(:movie, user: user) }
+
+      it 'decreases the likers of a movie' do
+        expect {subject}.to change { movie.likers.count }.by -1
+      end
+
+      it 'stops me being a liker of a movie' do
+        expect {subject}.to change {
+          movie.likers.include? user
+        }.from(true).to(false)
+      end
+
+      it 'decreases the number of movies I like' do
+        expect {subject}.to change { user.liked_movies.count }.by -1
+      end
+
+      it 'removes the movie from the list I like' do
+        expect {subject}.to change {
+          user.liked_movies.include? movie
+        }.from(true).to(false)
+      end
+    end
   end
   describe '#unvote'
 end
