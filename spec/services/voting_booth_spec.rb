f0bb6420fa903b3829054c356d79360a05f9b47c
Richard Brooke <ragnarcom@googlemail.com>
2021-02-16 10:16:09 +0000

    add first expectation of existing VotingBooth

--- spec/services/voting_booth_spec.rb
+++ spec/services/voting_booth_spec.rb
@@ -3,7 +3,10 @@ require 'rails_helper'
 RSpec.describe VotingBooth do
   describe '#vote' do
     context 'me liking a movie' do
-      it 'increases the likers of a movie'
+      let(:movie) { }
+      it 'increases the likers of a movie' do
+        expect(subject).to change(movie.likers.count).by 1
+      end
       it 'makes me a liker of a movie'
       it 'increases the number of movies I like'
       it 'adds the movie to the list I like'
